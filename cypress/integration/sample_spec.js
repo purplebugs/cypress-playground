7cbc6226f949ee402bc21bda665b0a11a8174a63
Anita Lipsky <anita@purplebugs.com>
2020-02-04 10:41:39 +0100

    Comment out pause command

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -2,7 +2,7 @@ describe('My First Test', function() {
     it('clicking "type" navigates to a new url', function() {
       cy.visit('https://example.cypress.io')
 
-      cy.pause()
+      // cy.pause()
 
       cy.contains('type').click()
 
